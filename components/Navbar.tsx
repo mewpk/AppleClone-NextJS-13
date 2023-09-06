b40fba0894c9cdd4319ea7d64a6c779958a9c78e
mewpk <milzaza123@gmail.com>
2023-09-06 20:50:40 +0700

    update : navbar rounded !

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -6,7 +6,7 @@ import CustomButton  from "./CustomButton"
 const Navbar = () => {
   return (
     <header className="w-full absolute z-10 ">
-        <nav className="max-w-[1440px] mx-auto flex justify-between items-center sm:px-16 px-6 py-4 bg-white align-middle">
+        <nav className="max-w-[1440px] mx-auto flex justify-between items-center sm:px-16 px-6 py-4 bg-white align-middle rounded-full">
             <Link href="/" >
                 <Image src="/logo-apple.svg" alt="logo" width={12} height={12} className="object-contain"></Image>
             </Link>
