8403fc49a6b3f6cbd00e589a4603c9f1041569f1
mewpk <milzaza123@gmail.com>
2023-09-06 18:55:07 +0700

    update : setup hero main page!

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,10 @@
+import { Hero } from "@/components";
 import Image from "next/image";
 
 export default function Home() {
   return (
-    <main className="overflow-hidden"></main>
+    <main className="overflow-hidden">
+      <Hero/>
+    </main>
   );
 }
---
+++ components/Hero.tsx
@@ -0,0 +1,12 @@
+import Image from "next/image";
+const Hero = () => {
+  return (
+    <div className="hero">
+      <h1 className="hero__title">Save on Mac or IPad for college.</h1>
+      <p>Plus get a gift card up to $150.</p>
+      <a href="">Shop now</a>
+    </div>
+  );
+};
+
+export default Hero;
